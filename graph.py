93eb9f47dfacf25d14a8a6c4c7c397910b827549
Akshay Raman <ar10021161@gmail.com>
2024-05-03 01:39:07 -0400

    added clipped traces graph

--- graph.py
+++ graph.py
@@ -6,17 +6,12 @@ import matplotlib.pyplot as plt
         
         
 
-df = pd.read_csv('history/2024-05-02_22:55:56_42_particle-v0_Contgae/data.csv')
-sns.lineplot(x='iteration', y='avg_ep_rew',data=df, label='lam = 0')
+df = pd.read_csv('history/2024-05-02_22:24:48_None_particle-v0_Contgae/data.csv')
+sns.lineplot(x='iteration', y='avg_ep_rew',data=df, label='Clipped Traces')
 
-df = pd.read_csv('history/2024-05-02_22:54:54_42_particle-v0_Contgae/data.csv')
-ax = sns.lineplot(x='iteration', y='avg_ep_rew',data=df, label='lam = 0.5')
+df = pd.read_csv('history/2024-05-02_22:42:48_40_particle-v0_Contgae/data.csv')
+ax = sns.lineplot(x='iteration', y='avg_ep_rew',data=df, label='Accumulating Traces')
 
-df = pd.read_csv('history/2024-05-02_22:50:41_13_particle-v0_Contgae/data.csv')
-ax = sns.lineplot(x='iteration', y='avg_ep_rew',data=df, label='lam = 0.9')
 
-df = pd.read_csv('history/2024-05-02_22:49:03_13_particle-v0_Contgae/data.csv')
-ax = sns.lineplot(x='iteration', y='avg_ep_rew',data=df, label='lam = 1.0')
-
-ax.set_title('Continual GAE - Lambda')
-plt.savefig('graphs/lambda.png')
+ax.set_title('Clipped Trace')
+plt.savefig('graphs/ppo.png')
